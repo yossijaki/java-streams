b529eda1b10aaba59a4090650eb4ef81bfdb57ad
Yossijaki Jorge Astilleros Chavez <yossijaki@gmail.com>
2024-05-21 17:45:03 -0600

    Lecture3 - getting min and max with streams min max function

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -13,11 +13,24 @@ public class Lecture3 {
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    Integer minimum = numbers.stream()
+        .min(Comparator.naturalOrder())
+        .get();
+
+    assertThat(minimum).isEqualTo(1);
+    System.out.println(minimum);
+
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
 
+    Integer maximum = numbers.stream()
+        .max(Comparator.naturalOrder())
+        .get();
+
+    assertThat(maximum).isEqualTo(100);
+    System.out.println(maximum);
   }
 }
